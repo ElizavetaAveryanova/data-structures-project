d0bc6c85a71aab88515d4ef53a0c012850f9fc88
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-01-11 13:48:27 +0500

    homework_1

--- src/stack.py
+++ src/stack.py
@@ -4,26 +4,24 @@ class Node:
     def __init__(self, data, next_node):
         """
         Конструктор класса Node
-
         :param data: данные, которые будут храниться в узле
         """
-        pass
-
+        self.data = data
+        self.next_node = next_node
 
 class Stack:
     """Класс для стека"""
-
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
         Метод для добавления элемента на вершину стека
-
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_top = Node(data, self.top)
+        self.top = new_top
 
     def pop(self):
         """
